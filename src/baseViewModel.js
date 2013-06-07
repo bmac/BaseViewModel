1585dbb9a174f3478710d18b98c7e364b63e4dd3
Brendan McLoughlin <bmac325@gmail.com>
2013-06-07 09:02:00 -0400

    add BaseViewModel code

---
+++ src/baseViewModel.js
@@ -0,0 +1,25 @@
+(function() {
+    var observableExtend = function(target, object) {
+        for (var key in object) {
+            if (object.hasOwnProperty(key)) {
+                target[key] = createObservable(object[key]);
+            }
+        }
+    };
+
+    var createObservable = function(value) {
+        if (Array.isArray(value)) {
+            return ko.observableArray(value);
+        }
+        return ko.observable(value);
+    };
+
+    var baseViewModel = Class.extend({
+        init: function(object) {
+            observableExtend(this, this.defaults);
+            observableExtend(this, object);
+        }        
+    });
+
+    window.BaseViewModel = baseViewModel;
+}());
